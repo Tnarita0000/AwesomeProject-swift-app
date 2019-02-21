c58a7e9188da73bcc359dbefa671930c23e4f9f5
Tnarita0000 <naritatakuya0000@gmail.com>
2019-02-21 22:11:27 +0900

    Initial Commit

---
+++ AwesomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeProject
+//
+//  Created by 成田拓哉 on 2019/02/21.
+//  Copyright © 2019 成田拓哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
